f2ceaed9ebaaaeea7ae92f80caf4faaa1b87e79e
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2023-03-07 09:21:31 +0100

    searchbar component was created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
+import useFetch, { IRecipe } from "../hooks/useFetch";
+import Searchbar from "./Searchbar";
 
 export default function Navbar() {
+  // const { recipes, error, isPending } = useFetch(
+  //   "http://localhost:3000/recipes"
+  // );
   return (
     <div className="navbar">
       <nav>
         <Link to="/" className="brand">
           <h1>RecipeHub</h1>
         </Link>
+        {/* {recipes && <Searchbar recipes={recipes as IRecipe[]} />} */}
+        <Searchbar />
 
         <Link to="/create">Create Recipe</Link>
       </nav>
