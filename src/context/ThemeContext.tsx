ba3f430db90ef503078992a0102fef64b234fcdf
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2023-03-07 19:02:57 +0100

    ThemeContext is being consumed by Navbar component

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,14 +1,18 @@
 import React, { createContext } from "react";
 import { useState } from "react";
 
-const ThemeContext = createContext({});
+type ThemeContextType = {
+  color: string;
+};
+
+const ThemeContext = createContext<ThemeContextType>({ color: "" });
 
 type Props = {
   children: string | JSX.Element | JSX.Element[];
 };
 
 export function ThemeProvider({ children }: Props) {
-  const [color, setColor] = useState("blue");
+  const [color, setColor] = useState<ThemeContextType>({ color: "blue" });
 
   return (
     <ThemeContext.Provider value={color}>{children}</ThemeContext.Provider>
