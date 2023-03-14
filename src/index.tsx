32a2bb369ef0ff866bd27fbc55b057681a076b3a
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2023-03-14 14:22:10 +0100

    fixing dependency issues

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
+
 ReactDOM.render(
   <StrictMode>
-    <ThemeProvider>
+    <ThemeProvider children={undefined}>
       <App />
     </ThemeProvider>
   </StrictMode>,
