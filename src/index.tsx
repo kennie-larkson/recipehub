d1e0c04e10f305ee92d2c1911d857b1d2b2f4cbd
Kehinde Lawal <kehindelawal@tts-MacBook-Pro.local>
2023-03-17 22:28:49 +0100

    fixing dependency errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
-import React, { StrictMode } from "react";
+import React from "react";
 
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
 
-ReactDOM.render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <ThemeProvider children={undefined}>
       <App />
     </ThemeProvider>
-  </StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+// ReactDOM.render(
+//   <StrictMode>
+//     <ThemeProvider children={undefined}>
+//       <App />
+//     </ThemeProvider>
+//   </StrictMode>,
+//   document.getElementById("root")
+// );
